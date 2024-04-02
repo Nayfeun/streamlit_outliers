38c0e6fb8884e405c83a8486e63a2b50cebc3ef5
Nathan Vaccaro <nac3pppoan@gmail.com>
2024-04-02 19:06:33 +0200

    regular outlier detection method are now properly working small tweaks in code

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from distributions.distributions import get_outlier_amount, get_data_points, get_threshold, get_full_distribution
 
+# Sample distributions and outliers
+file_path_distributions = os.path.join("src", "distributions.csv")
+file_path_outliers = os.path.join("src", "outliers.csv")
+
+distributions_df = pd.read_csv(file_path_distributions, sep=";", decimal=".")
+outliers_df = pd.read_csv(file_path_outliers, sep=";", decimal=",")
+
+# Define shape names for distributions and outliers
+distributions_shapes = distributions_df.columns
+outliers_shapes = outliers_df.columns
+
 
 def distribution_graph(distribution, w_mad, w_iqr, w_sd, const_mad, const_iqr, const_sd):
     # Set the background color and create a kernel density estimate plot without bars
@@ -38,6 +49,31 @@ def guess_separator(file_path):
             return sep
     return r'\t'
 
+
+def formula_choice(custom):
+    if not custom:
+        outlier_method = st.radio("Method", ['2.5 MAD', '1.5 IQR', '2.5 SD', '3.0 SD'])
+        if outlier_method.endswith('SD'):
+            user_mad_weight, user_iqr_weight, user_sd_weight = 0, 0, 1
+            user_mad_constant, user_iqr_constant, user_sd_constant = 0, 0, float(outlier_method[:3])
+        elif outlier_method.endswith('MAD'):
+            user_mad_weight, user_iqr_weight, user_sd_weight = 1, 0, 0
+            user_mad_constant, user_iqr_constant, user_sd_constant = float(outlier_method[:3]), 0, 0
+        else:
+            user_mad_weight, user_iqr_weight, user_sd_weight = 0, 1, 0
+            user_mad_constant, user_iqr_constant, user_sd_constant = 0, 1.5, 0
+
+    else:
+        user_mad_weight = st.number_input(label="MAD weight (%)", min_value=0, max_value=100, step=1) / 100
+        user_iqr_weight = st.number_input(label="IQR weight (%)", min_value=0, max_value=100, step=1) / 100
+        user_sd_weight = st.number_input(label="SD weight (%)", min_value=0, max_value=100, step=1) / 100
+
+        user_mad_constant = st.number_input(label="MAD constant", min_value=1.0, max_value=5.0, step=0.5)
+        user_iqr_constant = st.number_input(label="IQR constant", min_value=1.0, max_value=5.0, step=0.5)
+        user_sd_constant = st.number_input(label="SD constant", min_value=1.0, max_value=5.0, step=0.5)
+    return user_mad_weight, user_iqr_weight, user_sd_weight, user_mad_constant, user_iqr_constant, user_sd_constant
+
+
 def run():
     if user_file is None:
         user_full_distribution = get_full_distribution(distributions_df[user_distribution_shape.lower()],
@@ -52,39 +88,29 @@ def run():
     distribution_graph(user_full_distribution, user_mad_weight, user_iqr_weight, user_sd_weight, user_mad_constant,
                        user_iqr_constant, user_sd_constant)
 
+
 # Config
 st.set_page_config(
     page_title="Outlier detection",
     layout="centered",
 )
 
-
-
-# Sample distributions and outliers
-
-file_path_distributions = os.path.join("src_data", "distributions.csv")
-file_path_outliers = os.path.join("src_data", "outliers.csv")
-
-distributions_df = pd.read_csv(file_path_distributions, sep=";", decimal=".")
-outliers_df = pd.read_csv(file_path_outliers, sep=";", decimal=",")
-
-# Define shape names for distributions and outliers
-distributions_shapes = distributions_df.columns
-outliers_shapes = outliers_df.columns
-
 ### Tabs ###
 tab1, tab2 = st.tabs(['Simulation', 'Visualization'])
 
 with tab1:
+    st.write("### Distribution")
+    st.write("Choose the parameters of your distribution and outliers")
+
     # User distribution and outliers shape
-    user_distribution_shape = st.radio("Pick a distribution shape : ",
+    user_distribution_shape = st.radio("Distribution shape : ",
                                        [distribution_shape.title() for distribution_shape in distributions_shapes])
 
-    user_outliers_shape = st.radio("Pick an outliers shape: ",
+    user_outliers_shape = st.radio("Outliers shape: ",
                                    [outliers_shape.title().replace("_", " ") for outliers_shape in outliers_shapes])
 
-    user_distribution_size = st.slider(label="Pick a distribution size", min_value=1, max_value=1000)
-    user_outliers_rate = st.slider(label="Pick an outliers rate (%)", min_value=1, max_value=40) / 100
+    user_distribution_size = st.slider(label="Distribution size", min_value=1, max_value=1000)
+    user_outliers_rate = st.slider(label="Outliers rate (%)", min_value=1, max_value=40) / 100
 
 with tab2:
     user_file = st.file_uploader("Import CSV", type='csv')
@@ -95,22 +121,12 @@ with tab2:
         values = st.radio("Click on the column where the values are", [column for column in user_df.columns])
 
 # User formula choice
-st.write("## Formula")
+st.write("### Formula")
 st.write("Choose the parameters of the outlier detection formula")
 
 custom = st.checkbox("Custom")
 
-if not custom:
-    outlier_method = st.radio("Method", ['MAD', 'SD', 'IQR'])
-else:
-    user_mad_weight = st.number_input(label="MAD weight (%)", min_value=0, max_value=100, step=1) / 100
-    user_iqr_weight = st.number_input(label="IQR weight (%)", min_value=0, max_value=100, step=1) / 100
-    user_sd_weight = st.number_input(label="SD weight (%)", min_value=0, max_value=100, step=1) / 100
-
-    user_mad_constant = st.number_input(label="MAD constant", min_value=1.0, max_value=5.0, step=0.5)
-    user_iqr_constant = st.number_input(label="IQR constant", min_value=1.0, max_value=5.0, step=0.5)
-    user_sd_constant = st.number_input(label="SD constant", min_value=1.0, max_value=5.0, step=0.5)
+user_mad_weight, user_iqr_weight, user_sd_weight, user_mad_constant, user_iqr_constant, user_sd_constant = \
+    formula_choice(custom)
 
 run_button = st.button("Run", on_click=run)
-
-
--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def test_df_col_names():
-    distributions_df = pd.read_csv("src_data/distributions.csv", sep=";")
+    distributions_df = pd.read_csv("src/distributions.csv", sep=";")
     assert distributions_df.columns[0] == "normal"
 
 def test_mean():
